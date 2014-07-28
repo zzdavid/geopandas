a312cb1efa5c96963bf11a2b6390b71354799331
Kelsey Jordahl <kjordahl@enthought.com>
2014-07-28 11:49:50 -0700

    TST: Just use the astropy wheels

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-PANDAS_WHEELS=http://pandas.pydata.org/pandas-build/dev/wheels
 ASTROPY_WHEELS=http://wheels.astropy.org
 ASTROPY_WHEELS2=http://wheels2.astropy.org
 
-PIP_OPTIONS="-I --use-wheel --find-links=$PANDAS_WHEELS/$TRAVIS_PYTHON_VERSION --find-links=$ASTROPY_WHEELS --find-links=$ASTROPY_WHEELS2 --allow-external --allow-insecure"
+PIP_OPTIONS="-I --use-wheel --find-links=$ASTROPY_WHEELS --find-links=$ASTROPY_WHEELS2 --allow-external --allow-insecure"
 
 pip install $PIP_OPTIONS -r requirements.txt
 pip install $PIP_OPTIONS -r requirements.test.txt
